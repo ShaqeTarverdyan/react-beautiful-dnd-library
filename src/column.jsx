5d679389435753a7595fce1592c46771c2a547a5
shaqe <tshaqe@gmail.com>
2020-05-25 16:38:10 +0400

    learcn react-beautiful-dnd library

---
+++ src/column.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+
+import Task from "./task";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgray;
+  border-radius: 2px;
+  width: 220px;
+
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+  background-color: ${(props) =>
+    props.isDraggingOver ? "lightblue" : "white"};
+  flex-grow: 1;
+  min-height: 100px;
+`;
+
+const Column = ({ column, tasks }) => {
+  return (
+    <Container>
+      <Title>{column.title}</Title>
+      <Droppable droppableId={column.id}>
+        {(provided, snapshot) => (
+          <TaskList
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            isDraggingOver={snapshot.isDraggingOver}
+          >
+            {tasks.map((task, index) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </TaskList>
+        )}
+      </Droppable>
+    </Container>
+  );
+};
+
+export default Column;
